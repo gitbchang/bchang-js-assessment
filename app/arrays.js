d3c1033893f9738b776150720ec57e0f7a607a13
gitbchang <brian.h.chang1@gmail.com>
2017-03-20 23:16:00 -0500

    array test complete

--- app/arrays.js
+++ app/arrays.js
@@ -22,17 +22,11 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy: function(arr, item) {
-    // create a new Array for splice indexes
-    var newArray = [];
-    // get all the splice indexees
-    for(var i = 0; i < arr.length; i++){
-      if(arr[i] === item){
-        newArray.push(i);
+
+    for(var i = 0; i < arr.length;i++){
+      if(arr.indexOf(item) > -1){
+        arr.splice(arr.indexOf(item), 1);
       }
-    }
-    // go through the splice index array and remove item
-    for(var j = 0; j < newArray.length; j++){
-      arr.splice(newArray[j], 1);      
     }
 
     return arr;
@@ -44,38 +38,73 @@ exports.arraysAnswers = {
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    var newArray = arr1.concat(arr2);
+    return newArray;
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    var counter = 0;
+    for (var i = 0; i < arr.length; i++){
+      if(arr[i] === item){
+        counter++;
+      }
+    }
+    return counter;
   },
 
   duplicates: function(arr) {
+    var duplicates = [];
 
+    arr.forEach(function(element, index){
+      // find if there is a duplicate or not
+      if(arr.indexOf(element, index + 1) > -1){
+        // find if the element is already in the duplicate array or not
+        if(duplicates.indexOf(element) === -1){
+          duplicates.push(element);
+        }
+
+      }
+    });
+
+
+    return duplicates;
   },
 
   square: function(arr) {
-
+    for(var i = 0; i < arr.length; i++){
+      var squared = Math.pow(arr[i], 2);
+      arr.splice(i, 1, squared);
+    }
+    return arr;
   },
 
   findAllOccurrences: function(arr, target) {
-
+    var targetPositions = [];
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === target){
+        targetPositions.push(i);
+      }
+    }
+    return targetPositions;
   }
 };
