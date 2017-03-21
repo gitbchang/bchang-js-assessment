70fd957f03f3a5f93e3ff7fe8dfe13425a3b6b22
gitbchang <brian.h.chang1@gmail.com>
2017-03-21 00:53:49 -0500

    work

--- app/count.js
+++ app/count.js
@@ -2,6 +2,10 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    var countArray = [];
+    for(var i = start; i < end+1; i++){
+      countArray.push(i);
+    }
+    return countArray;
   }
 };
